70aae6ef8da9d8e5e8cc45c0ccefda27c5e9e87d
Michael Ernst <mernst@cs.washington.edu>
2026-02-07 11:43:48 -0800

    Add import

--- src/main/java/org/plumelib/options/OptionsDoclet.java
+++ src/main/java/org/plumelib/options/OptionsDoclet.java
@@ -22,6 +22,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UncheckedIOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
