03838db93aaa19a212c4bec9103b1916b18ad010
Michael Ernst <mernst@cs.washington.edu>
2026-06-02 10:09:22 -0700

    Update copied files

--- src/main/java/org/plumelib/options/OptionsDoclet.java
+++ src/main/java/org/plumelib/options/OptionsDoclet.java
@@ -633,7 +633,6 @@ public class OptionsDoclet implements Doclet {
    *
    * @return the docfile, but with the command-line argument documentation updated
    */
-  @SuppressWarnings("PMD.AssignmentInOperand") // BufferedReader does not support iterable
   @RequiresNonNull("docFile")
   private String newDocFileText() {
     StringJoiner b = new StringJoiner(lineSep);
