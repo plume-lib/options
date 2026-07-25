9778871e58604dbb221e0e6ad1a71815d126736c
Michael Ernst <mernst@cs.washington.edu>
2026-07-25 12:25:55 -0700

    Code review

--- src/main/java/org/plumelib/options/package-info.java
+++ src/main/java/org/plumelib/options/package-info.java
@@ -5,7 +5,7 @@
  *
  * <ul>
  *   <li>parses command-line options and sets fields in your program accordingly,
- *   <li>creates usage messages (such as printed by a `--help` option), and
+ *   <li>creates usage messages (such as printed by a {@code --help} option), and
  *   <li>(at build time) creates documentation suitable for a manual or manpage.
  * </ul>
  *
